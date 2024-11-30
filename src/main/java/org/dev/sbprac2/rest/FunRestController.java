c0598ddeeceb13d50d8898808b48024908ba59c5
Dev Hire <devhire8698@gmail.com>
2024-11-30 22:05:57 +0530

    clean up for command line app for JPA practice

--- src/main/java/org/dev/sbprac2/rest/FunRestController.java
+++ src/main/java/org/dev/sbprac2/rest/FunRestController.java
@@ -1,8 +1,6 @@
 package org.dev.sbprac2.rest;
 
 
-import org.dev.sbprac2.services.Teacher;
-import org.dev.sbprac2.services.thakurSir;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,14 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FunRestController {
 
-    Teacher Saswade;
-    Teacher ThakurSir;
-    @Autowired
-    public FunRestController(@Qualifier("SaswadeSir") Teacher Saswade
-    , @Qualifier("thakurSir") Teacher Thakur) {
-        this.Saswade = Saswade;
-        this.ThakurSir = Thakur;
-    }
+
 
     @Value("${my.name}")
     private String name;
@@ -29,25 +20,11 @@ public class FunRestController {
         return name;
     }
 
-    @GetMapping("/check")
+
+
+    @GetMapping("/getstudent")
     public String check() {
         return "are saswade and bhai are same = ";
-    }
-
-    //new end point for getting started
-    @GetMapping("/saswade")
-    public String workout() {
-        return Saswade.getTodaysSchedule();
-    }
-
-    @GetMapping("/thakur")
-    public String thakur() {
-        return ThakurSir.getTodaysSchedule();
-    }
-
-    @GetMapping("/fortune")
-    public String fortune() {
-        return "you are not lucky  ";
     }
 
 
---
+++ src/main/java/org/dev/sbprac2/services/StudentServices.java
@@ -0,0 +1,7 @@
+package org.dev.sbprac2.services;
+
+public interface StudentServices {
+
+    String GetStudentById(String id);
+
+}
