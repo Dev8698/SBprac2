172a10dcb6a507ef9c79a9add314e0093647e36c
Dev Hire <devhire8698@gmail.com>
2024-11-30 23:26:21 +0530

    entity package created

--- src/main/java/org/dev/sbprac2/SBprac2Application.java
+++ src/main/java/org/dev/sbprac2/SBprac2Application.java
@@ -1,7 +1,9 @@
 package org.dev.sbprac2;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SBprac2Application {
@@ -10,5 +12,11 @@ public class SBprac2Application {
         SpringApplication.run(SBprac2Application.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            System.out.println("Hello World!");
+        };
+    }
 
 }
